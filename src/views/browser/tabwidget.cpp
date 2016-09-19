aebfd2de72b08cff2b9a9eace4ae86c97cd1a95f
beunprepared <beunprepared@gmail.com>
2016-09-19 15:01:05 -0400

    void TabWidget::webViewTitleChanged(const QString &title)

--- src/views/browser/tabwidget.cpp
+++ src/views/browser/tabwidget.cpp
@@ -1709,7 +1709,8 @@ namespace browser {
 	int	index		= webViewIndex(webView);
 	auto	_real_title	= title.leftJustified(5, '.', true);
 	if(- 1 != index)setTabText(index, _real_title);
-	if(currentIndex() == index)emit setCurrentTitle(_real_title);	// "test"//
+	if(currentIndex() == index)emit setCurrentTitle(_real_title);	// "test"
+	//
 	if(_real_title != "")webView->page()->record_info_update(webView->page()->url(), _real_title);
 	sapp_t::historyManager()->updateHistoryItem(webView->page()->url(), title);
     }
