e3ae3dc2982a905f3c0ba40f46e0ae93a9404186
beimprovised <beunprepared@gmail.com>
2017-01-29 22:01:42 -0500

    first commit

--- src/libraries/crypt/pbkdf2qt.h
+++ src/libraries/crypt/pbkdf2qt.h
@@ -1,3 +1,4 @@
+<<<<<<< f95c9e39a5cec96b59701647fa10f1a16f28ba89
 #ifndef __PBKDF2QT_H__
 #define __PBKDF2QT_H__
 
@@ -46,3 +47,53 @@ private:
 };
 
 #endif // __PBKDF2QT_H__
+=======
+#ifndef __PBKDF2QT_H__
+#define __PBKDF2QT_H__
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+#include <QString>
+#include <QByteArray>
+
+#define PBKDF2QT_VERSION "Pbkdf2Qt Ver. 1.06 / 30.05.2011"
+
+#define PBKDF2QT_SHA1_BLOCK_LENGTH   64
+#define PBKDF2QT_SHA1_DIGEST_LENGTH  20
+#define PBKDF2QT_SALT_SIZE_MAX       1024
+
+
+class Pbkdf2Qt {
+
+public:
+    Pbkdf2Qt();
+    virtual ~Pbkdf2Qt();
+
+    const char *GetVersion(void);
+
+    QByteArray Pbkdf2(QByteArray pass,
+                      QByteArray salt,
+                      unsigned int rounds,
+                      unsigned int key_len);
+
+private:
+
+    unsigned int Min(unsigned int a, unsigned int b);
+
+    QByteArray HmacSha1(QByteArray text,
+                        QByteArray key);
+
+    int Pbkdf2_calculate(const char *pass,
+                         unsigned int pass_len,
+                         const char *salt,
+                         unsigned int salt_len,
+                         char *key,
+                         unsigned int key_len,
+                         unsigned int rounds);
+
+};
+
+#endif // __PBKDF2QT_H__
+>>>>>>> first commit
